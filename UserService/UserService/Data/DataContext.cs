8cd3847cff5798e6d7c5c076e757f4f7143e4f20
Tomás Malvissi <50890384+tomasmalvissi@users.noreply.github.com>
2020-08-16 17:02:36 -0300

    creacion de dbcontext (identity) y migracion

---
+++ UserService/UserService/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SharedModels.UserService;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace UserService.Data
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Empresa> Empresas { get; set; }
+        public DbSet<Persona> Persona { get; set; }
+    }
+}
